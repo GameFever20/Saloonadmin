aa1cf991d12bab7a410e9b5c85a8db21aff56fac
Aisha <priyank20patel@gmail.com>
2017-06-12 01:37:34 +0530

    MONA

--- app/src/main/java/utils/FireBaseHandler.java
+++ app/src/main/java/utils/FireBaseHandler.java
@@ -6,9 +6,14 @@ import android.util.Log;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by bunny on 11/06/17.
@@ -19,43 +24,68 @@ public class FireBaseHandler {
     private FirebaseDatabase mFirebaseDatabase;
 
 
-
     public FireBaseHandler() {
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
 
     }
 
-    public void uploadOrder(Order order){
-        mDatabaseRef= mFirebaseDatabase.getReference("Orders");
+    public void uploadOrder(Order order) {
+        mDatabaseRef = mFirebaseDatabase.getReference("Orders");
 
         mDatabaseRef.setValue(order).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                Log.d("On upload","upload completed");
+                Log.d("On upload", "upload completed");
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Log.d("On upload","upload failed");
+                Log.d("On upload", "upload failed");
 
             }
         });
     }
 
-    public void downloadOrder(){
+    public void downloadOrder() {
 
     }
 
-    public void uploadOrderList(){
+    public void uploadOrderList() {
 
     }
 
-    public void downloadOrderList(int limit){
+    public void downloadOrderList(int limit, int orderStatus) {
 
         DatabaseReference myRef = mFirebaseDatabase.getReference().child("Orders");
 
-        Query myref2 = myRef.orderByChild("orderStatus").equalTo(1).limitToFirst(limit);
+        Query myref2 = myRef.orderByChild("orderStatus").equalTo(orderStatus).limitToFirst(limit);
+
+        myref2.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList<Order> ordersArrayList = new ArrayList<Order>();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Order order = snapshot.getValue(Order.class);
+                    ordersArrayList.add(order);
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    public interface DataBaseHandlerOrderListListner {
+
+        public void onOrderList(ArrayList<Order> ordersArrayList);
 
 
     }
