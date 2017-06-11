b59427500c580a83041f4b8e279930a785929a7b
Aisha <priyank20patel@gmail.com>
2017-06-12 00:24:23 +0530

    MONA

--- app/src/main/java/app/admin/saloon/craftystudio/saloonadmin/LoginActivity.java
+++ app/src/main/java/app/admin/saloon/craftystudio/saloonadmin/LoginActivity.java
@@ -1,15 +1,37 @@
 package app.admin.saloon.craftystudio.saloonadmin;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static app.admin.saloon.craftystudio.saloonadmin.R.styleable.Toolbar;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText mEmailEditText, mPasswordEditText;
+    Button mSignInButton;
+    ProgressDialog progressDialog;
+
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +39,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Instantiating views and buttons
+        mEmailEditText = (EditText) findViewById(R.id.login_Email_EditText);
+        mPasswordEditText = (EditText) findViewById(R.id.login_Password_EditText);
+        mSignInButton = (Button) findViewById(R.id.login_signIn_Button);
+
+
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,4 +59,89 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void signIn(String email, String password) {
+        Log.d("In sign in ", "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("In sign in ", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                             updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("In sign in", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                             updateUI(null);
+                        }
+
+
+                         hideProgressDialog();
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        // updateUI(null);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressDialog();
+        if (user != null) {
+        } else {
+        }
+    }
+
+    public void showProgressDialog() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Signing in..");
+        progressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+
+        progressDialog.cancel();
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailEditText.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailEditText.setError("Required.");
+            valid = false;
+        } else {
+            mEmailEditText.setError(null);
+        }
+
+        String password = mPasswordEditText.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordEditText.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordEditText.setError(null);
+        }
+
+        return valid;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
 }
