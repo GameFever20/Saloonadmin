0d7a6f802d9b48a876399d3ae5216acb8ae5307f
bunny <gamefever20@gmal.com>
2017-06-11 23:15:38 +0530

    Initial configuration

--- app/src/main/java/utils/FireBaseHandler.java
+++ app/src/main/java/utils/FireBaseHandler.java
@@ -1,13 +1,20 @@
 package utils;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by bunny on 11/06/17.
  */
 
 public class FireBaseHandler {
+    private DatabaseReference mDatabase;
+
 
     public FireBaseHandler() {
         //hello he
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
     }
 
     public void uploadOrder(){
